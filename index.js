d4fecc4a2b6d5e41f1dddebcb1061f9ec7b82f67
Bamidele Damilola <bamideledamilola3@gmail.com>
2022-11-14 15:27:03 -0800

    added some images and minor changes

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const members = [
   {
     name: "Bamidele Damilola Joseph",
     matric: "1909003030",
+    address: "blue lodge, irewumni community, adoe-ekiti",
+    image: "",
   },
   {
-    name: "this nigga is a muthafuckker",
-    matric: "eat my shit",
+    name: "owoeye oluwashina samuel",
+    matric: "1909003073",
+    address: "baddo lodge, unity iworoko",
+    image: "",
+  },
+  {
+    name: "esan favour olaniyi",
+    matric: "1909003038",
+    address: "adeparusi street, opp dave hotel, adebayo",
+    image: "favour.jfif",
   },
 ];
 
@@ -13,12 +23,12 @@ const membersEl = document.querySelector(".members");
 for (const member of members) {
   const stringElement = `
     <section class="member">
-      <img src="./images/eksu-logo.jpg" />
+      <img src="./images/${member.image ? member.image : "placeholder.jpg"}" />
       <div class="details">
         <p class="name">${member.name}</p>
         <p class="address">${member.address ? member.address : "null"}</p>
         <p class="matric">${member.matric}</p>
-        <p class="cgpa">${member.cgpa ? member.cgpa : "null"}</p>
+        <p class="cgpa">${member.cgpa ? member.cgpa : "5.0"}</p>
       </div>
     </section>`;
   membersEl.insertAdjacentHTML("beforeend", stringElement);
