7e7593c0e0f2f2840e9bd73f05a1ae7adca47206
Bamidele Damilola <bamideledamilola3@gmail.com>
2022-11-14 14:57:52 -0800

    finishing touches

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("hi there");
-});
+const members = [
+  {
+    name: "Bamidele Damilola Joseph",
+    matric: "1909003030",
+  },
+  {
+    name: "this nigga is a muthafuckker",
+    matric: "eat my shit",
+  },
+];
+
+const membersEl = document.querySelector(".members");
+for (const member of members) {
+  const stringElement = `
+    <section class="member">
+      <img src="./images/eksu-logo.jpg" />
+      <div class="details">
+        <p class="name">${member.name}</p>
+        <p class="address">${member.address ? member.address : "null"}</p>
+        <p class="matric">${member.matric}</p>
+        <p class="cgpa">${member.cgpa ? member.cgpa : "null"}</p>
+      </div>
+    </section>`;
+  membersEl.insertAdjacentHTML("beforeend", stringElement);
+}
