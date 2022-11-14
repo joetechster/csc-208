66b387ba3a382250e1a0784eff64714856030599
Bidexdablitz <67684856+Bidexdablitz@users.noreply.github.com>
2022-11-14 18:42:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,60 @@ const members = [
     address: "adeparusi street, opp dave hotel, adebayo",
     image: "favour.jfif",
   },
+{
+    name: "Olajide Olalekan Simon",
+    matric: "1808003090",
+    address: "phase 2 osekita",
+    image: "",
+  },
+{
+    name: "ADEWUMI AYODELE AKEEM",
+    matric: "1909003005",
+    address: "Are road 8 iworoko",
+    image: "",
+  },
+{ 
+    name: "Onibaba OLADAMOLA",
+    matric: "1909003070",
+    address: "Alao French centre road, Adehun",
+    image: "",
+  },
+{
+    name: "Sunday Oloruntobi John",
+    matric: "1909003080",
+    address: "Lungi, Maitama, AMAC",
+    image: "",
+  },
+{
+    name: "Akinola Tosin Samuel",
+    matric: "178862120",
+    address: "Macedonia Hostel, Satellite, EKSU, Ado-Ekiti State",
+    image: "",
+  },
+{
+    name: "Akinsanya Oluwaseun Kemi",
+    matric: "1909003015",
+    address: "Anglican Hostel, 2 in 1",
+    image: "",
+  },
+{  
+    name: "Abdulraheem Habib Onimisi",
+    matric: "2009003002",
+    address: "No 5 Midas Avenue",
+    image: "",
+  },
+{
+    name: "Fagbohun Olamide Esther",
+    matric: "168870075",
+    address: "12, Onishinla GRA, Ado Ekiti",
+    image: "",
+  },
+{
+    name: "OWOEYE OLUWATOBI ISAAC",
+    matric: "1909003074",
+    address: "Are road 2",
+    image: "",
+  },
 ];
 
 const membersEl = document.querySelector(".members");
